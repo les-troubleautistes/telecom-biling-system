266e77b70615e83d117eafe6af3f6a5453088383
Kevan <selk11@proton.me>
2026-05-06 00:46:13 +0200

    norminette fix nb. 1

--- src/main.h
+++ src/main.h
@@ -6,12 +6,12 @@
 /*   By: alexis_marechal <michalex37@proton.me>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 23:32:12 by alexis_marech     #+#    #+#             */
-/*   Updated: 2026/05/06 00:32:19 by selkij           ###   ########.fr       */
+/*   Updated: 2026/05/06 00:42:11 by selkij           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
-#define MAIN_H
+# define MAIN_H
 
 void	delete_record(void);
 int		view_payment(void);
