a9d860cc1ffccce35e3fcc298853c31464e2fae4
Kevan <selk11@proton.me>
2026-05-06 00:32:56 +0200

    Add include guard to header

--- src/main.h
+++ src/main.h
@@ -6,9 +6,14 @@
 /*   By: alexis_marechal <michalex37@proton.me>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 23:32:12 by alexis_marech     #+#    #+#             */
-/*   Updated: 2026/05/05 23:38:14 by alexis_marech    ###   ########.fr       */
+/*   Updated: 2026/05/06 00:32:19 by selkij           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MAIN_H
+#define MAIN_H
+
 void	delete_record(void);
 int		view_payment(void);
+
+#endif // MAIN_H
