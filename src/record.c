8fe556636f9117d2056468f9354917e0fc65418a
Kevan <selk11@proton.me>
2026-05-06 00:51:39 +0200

    base search record with a norminette error

--- src/record.c
+++ src/record.c
@@ -6,15 +6,20 @@
 /*   By: alexis_marechal <michalex37@proton.me>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 23:16:03 by alexis_marech     #+#    #+#             */
-/*   Updated: 2026/05/05 23:30:17 by alexis_marech    ###   ########.fr       */
+/*   Updated: 2026/05/06 00:51:00 by selkij           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+void	search_record()
+{
+	printf("RECORD SEARCHED BIP BOUP\n");
+}
+
 //delete record btw
 void	delete_record(void)
 {
-	printf("record deleted");
+	printf("record deleted\n");
 }
