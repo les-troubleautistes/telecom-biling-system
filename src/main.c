1bab55c63677f27c1411effbead53982bc6d5512
mich hatake <michalex37@proton.me>
2026-05-05 23:12:27 +0200

    42 norminette refactor

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 /*   By: alexis_marechal <michalex37@proton.me>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 23:09:21 by alexis_marech     #+#    #+#             */
-/*   Updated: 2026/05/05 23:09:21 by alexis_marech    ###   ########.fr       */
+/*   Updated: 2026/05/05 23:12:10 by alexis_marech    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	return EXIT_SUCCESS;
+	return (0);
 }
